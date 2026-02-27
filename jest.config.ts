6522e54e8dd7dbb4e4c63f6a816763f5be74dfdd
kuroro <fernandovalenzuelaibarra@gmail.com>
2026-02-27 12:07:56 -0300

    feat: add coverage threshold configuration to Jest setup

--- jest.config.ts
+++ jest.config.ts
@@ -19,6 +19,14 @@ const config: Config = {
     "!app/**/*.d.ts",
   ],
   coverageReporters: ["lcov", "text", "text-summary"],
+  coverageThreshold: {
+    global: {
+      branches: 0,
+      functions: 0,
+      lines: 0,
+      statements: 0,
+    },
+  },
 };
 
 export default createJestConfig(config);
